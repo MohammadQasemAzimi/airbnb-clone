59d358df6cbd16a2e8cf8d05b9060ecd0ecff5d5
yosefg-dev <jossmicheal@gmail.com>
2022-12-01 16:43:57 +0100

    changing the components

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,29 @@
 import flatsController from '../controllers/flatsController'
 import Card from '../components/Card'
+import Head from 'next/head';
+
+//import React from "react";
+
+// reactstrap components
+import { Container, Row, Col } from "reactstrap";
+
 export default function Home(props) {
 
   //console.log('result of props is ', props);
   const flats = props.flats;
   return (
     <>
-      <h1>Empty Next App</h1>
-      {
-      flats.map(flat => (
-      <Card flat={flat} key={flat.id}/>
-      ))
-      }      
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+        />
+      </Head>
+      <Container>
+        <Row>
+          {flats.map(flat => (<Col key={flat.id}><Card flat={flat} key={flat.id} /></Col>))}
+        </Row>
+      </Container>
     </>
   )
 }
@@ -21,5 +33,4 @@ export async function getServerSideProps(req, res) {
   return {
     props: { flats },
   }
- }
- 
+}
