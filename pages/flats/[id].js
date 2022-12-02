3f949ec483e54380336e2c4e671c455779abd4db
yeshey choden <yeshey@DESKTOP-ES2KOVM.localdomain>
2022-12-02 12:59:48 +0100

    slect one and nav bar

--- pages/flats/[id].js
+++ pages/flats/[id].js
@@ -1,6 +1,5 @@
 import styles from '../../styles/Home.module.css'
 import flatsController from '../../controllers/flatsController'
-<<<<<<< HEAD
 import Navbar from '../../components/navbar'
 
 export default function selectOne(props) {
@@ -22,27 +21,6 @@ export default function selectOne(props) {
           <h6>{flats.location}</h6>
           <h6>{flats.codeNo}</h6>
           <h6>{flats.price}</h6>
-=======
-import Image from 'next/image'
-//import Navbar from '../../components/navbar'
-
-export default function selectOne(props) {
-  const flat = props.flat
-
-  return (
-    <>      
-      <div class={styles.container}>
-        <div class="child">
-          <Image src={flat.imageurl} width='500' height='500' alt=''/>
-        </div>
-        <div class="child">Owner Details</div>
-        <div class="child" >Flat details
-          <h5>{flat.title}</h5>
-          <h6>{flat.description}</h6>
-          <h6>{flat.location}</h6>
-          <h6>{flat.codeNo}</h6>
-          <h6>{flat.price}</h6>
->>>>>>> 2bacfef0c987bb5891f1c076b7aa5233c8f94244
         </div>
 
         
