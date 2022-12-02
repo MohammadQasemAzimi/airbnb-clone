8459322cad6aa7ec5482a77776155c1afc54cce0
Asadullah Hassani <asadullah.hassani293@gmail.com>
2022-12-02 16:53:41 +0100

    visitor details

--- pages/bookings/vistor.js
+++ pages/bookings/vistor.js
@@ -1,18 +1,23 @@
 import bookingController from '../../controllers/bookingController'
 import styles from '../../styles/Home.module.css'
 import Card from '../../components/Card'
+import flatsController from '../../controllers/flatsController'
 //import Navbar from '../../components/navbar'
 export default function selectOne(props) {
 
     const vistorBookings = props.bookings
     console.log(vistorBookings)
+    console.log(props)
     return (
+     
         <div>
             {vistorBookings.map(vistorBooking => (
-                <h2 key={vistorBooking.id} >userId: {vistorBooking.UserId}, FlatId: {vistorBooking.FlatId}</h2>
+                <h2 key={vistorBooking.id} >userId: {vistorBooking.UserId}, FlatId: {vistorBooking.FlatId},</h2>
 
             ))}
         </div>
+       
+        
     )
 }
 export async function getServerSideProps(req, res) {
@@ -21,6 +26,7 @@ export async function getServerSideProps(req, res) {
     // get all the booking with the vistor ID 
     const vistorID = 1
     const bookings = await bookingController.findVistorBookings(vistorID)
+    //const bookings = await flatsController.findUserFlats(vistorID)
     return {
         props: { bookings },
     }
